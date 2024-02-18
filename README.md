93b66d3fa6a4fb77f556bbad2006becc18bd14d7
Felipe Trost <felipe.trost@gmail.com>
2024-02-18 15:42:59 +0100

    initial commit 🚀

---
+++ README.md
@@ -0,0 +1,12 @@
+# Kakaroto
+
+## Goals
+
+- [ ] Multiple player games
+- [ ] Customizable question collections for each game
+- [ ] Questions can be skipped
+- [ ] Keeping track of scores for those who completed the penalties -> winners and losers
+- [ ] Maintain a uniform distribution of participants
+- [ ] Maintain a uniform distribution of penalties
+- [ ] Questions include games
+- [ ] (UI) One color for each participant
---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main>
+      <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20">
+        <div className="flex flex-1 flex-col items-center gap-4 text-center lg:gap-8">
+          <div className="space-y-4">
+            <h1 className="text-4xl font-bold lg:text-6xl">Kakaroto 🍺</h1>
+            <h2 className="text-lg font-light text-muted-foreground lg:text-3xl">
+              El mejor juego
+            </h2>
+          </div>
+          <Link
+            href="https://google.com"
+            className={`w-[10rem] ${cn(buttonVariants({ size: "lg" }))}`}
+          >
+            Get started
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+}
