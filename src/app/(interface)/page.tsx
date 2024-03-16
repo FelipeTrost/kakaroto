0db9d561f3d75c48f686643c431b662a0f501a43
Felipe Trost <felipe.trost@gmail.com>
2024-03-16 16:00:21 +0100

    feat: allways show create collection 👀

--- src/app/(interface)/page.tsx
+++ src/app/(interface)/page.tsx
@@ -1,11 +1,8 @@
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { getServerAuthSession } from "@/server/auth";
 import Link from "next/link";
 
 export default async function HomePage() {
-  const session = await getServerAuthSession();
-  const user = session?.user;
   return (
     <main>
       <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20">
@@ -13,7 +10,7 @@ export default async function HomePage() {
           <div className="space-y-4">
             <h1 className="text-4xl font-bold lg:text-6xl">Kakaroto 🍺</h1>
             <h2 className="text-lg font-light text-muted-foreground lg:text-3xl">
-              El mejor juego
+              A lot of fun
             </h2>
           </div>
           <Link
@@ -22,14 +19,12 @@ export default async function HomePage() {
           >
             Get started
           </Link>
-          {user && (
-            <Link
-              href="/create-collection"
-              className={`w-[10rem] ${cn(buttonVariants({ size: "lg", variant: "outline" }))}`}
-            >
-              Create Collection
-            </Link>
-          )}
+          <Link
+            href="/create-collection"
+            className={`w-[10rem] ${cn(buttonVariants({ size: "lg", variant: "outline" }))}`}
+          >
+            Create Collection
+          </Link>
         </div>
       </section>
     </main>
