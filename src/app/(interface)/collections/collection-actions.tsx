5d18efb9526b5c15702afb3c5f33279d00b1a048
Felipe Trost <felipe.trost@gmail.com>
2024-09-02 21:55:08 +0200

    feat: app icons 🗺️

--- src/app/(interface)/collections/collection-actions.tsx
+++ src/app/(interface)/collections/collection-actions.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { type questionCollections } from "@/server/db/schema";
-import { MdDelete, MdEdit } from "react-icons/md";
 import { useTransition } from "react";
 import { deleteCollection as _deleteCollection } from "@/server/db/actions";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { DeleteIcon, EditIcon } from "@/components/kakaroto/icons";
 
 export default function CollectionActions({
   collection,
@@ -26,11 +26,11 @@ export default function CollectionActions({
     <div className="flex flex-row gap-4">
       <Link href={`/collections/${collection.id}`}>
         <Button variant="secondary">
-          <MdEdit className="h-3 w-3" />
+          <EditIcon className="h-3 w-3" />
         </Button>
       </Link>
       <Button onClick={deleteCollection} disabled={deleting}>
-        <MdDelete className="h-3 w-3" />
+        <DeleteIcon className="h-3 w-3" />
       </Button>
     </div>
   );
---
+++ src/components/kakaroto/icons.tsx
@@ -0,0 +1,4 @@
+import { MdDelete, MdEdit } from "react-icons/md";
+
+export const DeleteIcon = MdDelete;
+export const EditIcon = MdEdit;
