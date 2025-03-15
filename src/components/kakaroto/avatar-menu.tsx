ee4ed754b858a069d3371d9b87e156db06f8afad
Felipe Trost <felipe.trost@gmail.com>
2025-03-15 18:07:03 +0100

    feat: delete account

--- src/components/kakaroto/avatar-menu.tsx
+++ src/components/kakaroto/avatar-menu.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -12,36 +12,147 @@ import {
 import { type DefaultSession } from "next-auth";
 import { signOut } from "next-auth/react";
 import { HiOutlineLogout } from "react-icons/hi";
+import { MdDelete } from "react-icons/md";
+import { useToast } from "@/components/ui/use-toast";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import Link from "next/link";
+import { useState } from "react";
+import { buttonVariants } from "../ui/button";
+import { deleteUser } from "@/server/db/user-actions";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { isUserResponse } from "@/server/user-response";
 
-export default function AvatarMenu({ user, optionalComponentsClasses }: { user: NonNullable<DefaultSession['user']>, optionalComponentsClasses?: string }) {
+const LoadingSpinner = ({ className }: { className?: string }) => {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src={user.image ?? undefined} />
-          <AvatarFallback>{user.name?.substring(0, 2)}</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => signOut()}>
-          <HiOutlineLogout className="mr-2" />
-          Sign out
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild className={optionalComponentsClasses}>
-          <Link href="/collections">
-            My collections
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild className={optionalComponentsClasses}>
-          <Link href="/collections/new">
-            New collection
-          </Link>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu >
-  )
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("animate-spin", className)}
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    </svg>
+  );
+};
+
+export default function AvatarMenu({
+  user,
+  optionalComponentsClasses,
+}: {
+  user: NonNullable<DefaultSession["user"]>;
+  optionalComponentsClasses?: string;
+}) {
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const [deleteAccountOpen, setDeleteAccountOpen] = useState(false);
+  const [deleting, setDeleting] = useState(false);
+
+  async function deleteAccount() {
+    setDeleting(true);
+    try {
+      const res = await deleteUser();
+      if (isUserResponse(res)) throw res;
+      await signOut({ redirect: false });
+      toast({ description: "Account deleted" });
+      router.push("/");
+    } catch (e) {
+      if (isUserResponse(e))
+        toast({
+          title: "Error",
+          description: e.message as string,
+          variant: "destructive",
+        });
+    }
+
+    setDeleting(false);
+  }
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarImage src={user.image ?? undefined} />
+            <AvatarFallback>{user.name?.substring(0, 2)}</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Collections</DropdownMenuLabel>
+          <DropdownMenuItem asChild className={optionalComponentsClasses}>
+            <Link href="/collections">My collections</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild className={optionalComponentsClasses}>
+            <Link href="/collections/new">New collection</Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuItem
+            onClick={() => signOut({ redirect: true, callbackUrl: "/" })}
+          >
+            <HiOutlineLogout className="mr-2" />
+            Sign out
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="text-destructive hover:text-destructive"
+            onClick={() => setDeleteAccountOpen(true)}
+          >
+            <MdDelete className="mr-2" />
+            Delete account
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <AlertDialog
+        open={deleteAccountOpen}
+        onOpenChange={(open) => setDeleteAccountOpen(open)}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Delete Account?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and your collections.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              className={buttonVariants({ variant: "destructive" })}
+              onClick={async (e) => {
+                e.preventDefault();
+                await deleteAccount();
+                setDeleteAccountOpen(false);
+              }}
+            >
+              <LoadingSpinner
+                className={cn([
+                  "inline-flex h-4 w-4 transition-all duration-300",
+                  { "mr-0 w-0 opacity-0": !deleting },
+                  { "opacity-1 mr-2 w-full": deleting },
+                ])}
+              />
+              Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
 }
---
+++ src/server/db/user-actions.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { db } from ".";
+import { pinnedCollections, questionCollections, users } from "./schema";
+import { getServerAuthSession } from "../auth";
+import { userResponse } from "../user-response";
+import { eq, sql } from "drizzle-orm";
+
+export async function deleteUser() {
+  const session = await getServerAuthSession();
+
+  if (!session) return userResponse("error", "You must be logged in");
+  const userId = session.user.id;
+
+  try {
+    const usersDeleted = await db.transaction(async (tx) => {
+      await tx.execute(sql`
+        DELETE FROM ${pinnedCollections}
+        WHERE ${pinnedCollections.id} in 
+        (SELECT ${questionCollections.id} FROM ${questionCollections} WHERE ${questionCollections.userId} = ${userId})
+      `);
+
+      await tx
+        .delete(questionCollections)
+        .where(eq(questionCollections.userId, userId));
+
+      const userResult = await tx
+        .delete(users)
+        .where(eq(users.id, userId))
+        .returning({ id: users.id });
+
+      return userResult.length;
+    });
+    if (usersDeleted === 0) return userResponse("error", "User not found");
+  } catch (error) {
+    console.error(error);
+    return userResponse("error", "Something went wrong");
+  }
+}
