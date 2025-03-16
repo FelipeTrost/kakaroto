3f6e638f10215a7cc96df3723eb413ba0e25e2cc
Felipe Trost <felipe.trost@gmail.com>
2025-03-16 21:41:51 +0100

    feat: confirmation modal for deleting collection

--- src/app/(interface)/collections/collection-actions.tsx
+++ src/app/(interface)/collections/collection-actions.tsx
@@ -5,8 +5,20 @@ import { useTransition } from "react";
 import { deleteCollection as _deleteCollection } from "@/server/db/collection-actions";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { DeleteIcon, EditIcon } from "@/components/kakaroto/icons";
+import {
+  DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
+import { LoadingSpinner } from "@/components/kakaroto/spinner";
 
 export default function CollectionActions({
   collection,
@@ -29,9 +41,35 @@ export default function CollectionActions({
           <EditIcon className="h-3 w-3" />
         </Button>
       </Link>
-      <Button onClick={deleteCollection} disabled={deleting}>
-        <DeleteIcon className="h-3 w-3" />
-      </Button>
+
+      <Dialog>
+        <DialogTrigger className={buttonVariants()} disabled={deleting}>
+          <LoadingSpinner
+            className={cn([
+              "mr-0 inline-flex h-4 w-0 opacity-0 transition-all duration-300",
+              { "opacity-1 mr-2 w-fit": deleting },
+            ])}
+          />
+          <DeleteIcon className="h-3 w-3" />
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete Collection?</DialogTitle>
+            <DialogDescription>This action cannot be undone.</DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose className={buttonVariants({ variant: "outline" })}>
+              Cancel
+            </DialogClose>
+            <DialogClose
+              className={buttonVariants({ variant: "destructive" })}
+              onClick={deleteCollection}
+            >
+              Delete
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
