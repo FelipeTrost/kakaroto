eafc6dcc2875f6c50e2080c465a9f2426cc86857
Felipe Trost <felipe.trost@gmail.com>
2025-03-27 17:58:08 +0100

    fix: sort players when parsing question

--- src/lib/game/parser.ts
+++ src/lib/game/parser.ts
@@ -1,54 +1,62 @@
 // TODO: refactor this file
 
-export type ParsedQuestion =
-  {
-    /** Strings represent a part of the question and numbers represent different players */
-    parts: (string | number)[]
-    /** The number that were used to reference players sorted */
-    players: number[]
-    nPlayers: number
-  }
+export type ParsedQuestion = {
+  /** Strings represent a part of the question and numbers represent different players */
+  parts: (string | number)[];
+  /** The number that were used to reference players sorted */
+  players: number[];
+  nPlayers: number;
+};
 
 export function parseQuestion(input: string) {
   // Sorted players
-  const playerMatches = input.matchAll(/\$\d+/g)
+  const playerMatches = input.matchAll(/\$\d+/g);
 
   const players = new Set<number>();
 
   for (const player of playerMatches) {
-    const playerNo = Number.parseInt(player[0].substring(1))
-    players.add(playerNo)
+    const playerNo = Number.parseInt(player[0].substring(1));
+    players.add(playerNo);
   }
 
-  const sortedPlayers = [...players.values()].sort()
+  const sortedPlayers = [...players.values()];
+  sortedPlayers.sort((a, b) => a - b);
 
   // TODO: move this to another function
   // Split challenge on `$<num>`
-  const parts: (string | number)[] = []
+  const parts: (string | number)[] = [];
   let lastIdx = 0;
 
   for (const player of input.matchAll(/\$\d+/g)) {
-    const idx = player.index ?? 0
-    if (idx > 0) parts.push(input.substring(lastIdx, idx))
-    lastIdx = idx + player[0].length
+    const idx = player.index ?? 0;
+    if (idx > 0) parts.push(input.substring(lastIdx, idx));
+    lastIdx = idx + player[0].length;
 
-    const playerNo = Number.parseInt(player[0].substring(1))
-    parts.push(sortedPlayers.indexOf(playerNo) + 1)
+    const playerNo = Number.parseInt(player[0].substring(1));
+    parts.push(sortedPlayers.indexOf(playerNo) + 1);
   }
-  parts.push(input.substring(lastIdx,))
+  parts.push(input.substring(lastIdx));
 
-  return { parts, nPlayers: sortedPlayers.length, players: sortedPlayers, playersSet: players } as ParsedQuestion
+  const a = {
+    parts,
+    nPlayers: sortedPlayers.length,
+    players: sortedPlayers,
+    playersSet: players,
+  };
+  console.log("parse", a);
+  return a as ParsedQuestion;
 }
 
 // returns a list in order to get same number-> player mapping for ongoing challenges
 export function displayQuestion(challenges: string[], players: string[]) {
-  const displayQuestions = []
+  const displayQuestions = [];
 
   // Get challenge with most players
   let mostPlayers: number[] = [];
   for (const challenge of challenges) {
     const parsed = parseQuestion(challenge);
-    if (parsed.players.length > mostPlayers.length) mostPlayers = parsed.players;
+    if (parsed.players.length > mostPlayers.length)
+      mostPlayers = parsed.players;
   }
 
   // Compute mapping
