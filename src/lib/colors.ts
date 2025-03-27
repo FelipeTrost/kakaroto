282b81e4b8ebcc8987fe176e109a4184f5a3f64b
Felipe Trost <felipe.trost@gmail.com>
2025-03-27 17:57:19 +0100

    feat: default palette for colors

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,4 +1,17 @@
+const defaultPalette = [
+  "#ffadadff",
+  "#ffd6a5ff",
+  "#caffbfff",
+  "#9bf6ffff",
+  "#a0c4ffff",
+  "#bdb2ffff",
+  "#ffc6ffff",
+];
+
+/** please don't do in place ops with the returned array (this is a note for myself) */
 export function generateColors(n: number, saturation = 100, lightness = 50) {
+  if (n <= defaultPalette.length) return defaultPalette;
+
   const colors = [];
 
   const step = 360 / n;
