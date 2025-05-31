6ecf84f28dff93bafa3631550777078d82601c9e
Felipe Trost <felipe.trost@gmail.com>
2025-05-31 15:10:25 +0200

    bouncy div doesn't change width
    there were some weird animation effects because bouncy div changed the
    width of the div

--- src/components/kakaroto/bouncy-div.tsx
+++ src/components/kakaroto/bouncy-div.tsx
@@ -4,36 +4,39 @@ import { type ComponentProps } from "react";
 const divVariants = {
   hidden: {
     opacity: 0,
-    scale: 0.8,
-    y: 50
+    y: 50,
+    rotateX: -15,
   },
   visible: {
     opacity: 1,
-    scale: 1,
     y: 0,
+    rotateX: 0,
     transition: {
       duration: 0.5,
       type: "spring",
-      stiffness: 120
-    }
+      stiffness: 120,
+    },
   },
   exit: {
     opacity: 0,
-    scale: 0.5,
     y: 50,
+    rotateX: 15,
     transition: {
-      duration: 0.3
-    }
-  }
+      duration: 0.3,
+    },
+  },
 };
 
-export default function BouncyDiv(props: ComponentProps<typeof motion["div"]>) {
-  return <motion.div
-    variants={divVariants}
-    initial="hidden"
-    animate="visible"
-    exit="exit"
-    {...props}
-  />
-
+export default function BouncyDiv(
+  props: ComponentProps<(typeof motion)["div"]>,
+) {
+  return (
+    <motion.div
+      variants={divVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      {...props}
+    />
+  );
 }
