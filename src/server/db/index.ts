04cb64683f46c0318dab21b1f2aab56d236b6a0a
felipe <felipe.trost@gmail.com>
2025-12-27 13:40:56 +0100

    debug log

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,6 +3,7 @@ import * as schema from "./schema";
 import { env } from "@/env.js";
 import postgres from "postgres";
 
+console.log("connecting to db with", env.DATABASE_URL);
 export const connection = postgres(env.DATABASE_URL);
 
 export const db = drizzle(connection, { schema });
