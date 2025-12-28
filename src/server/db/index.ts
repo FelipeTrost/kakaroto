3cdbfeb0757fcfd92c924dd986e201626453f45b
felipe <felipe.trost@gmail.com>
2025-12-28 01:01:59 +0100

    Revert "debug log"
    This reverts commit 04cb64683f46c0318dab21b1f2aab56d236b6a0a.

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,7 +3,6 @@ import * as schema from "./schema";
 import { env } from "@/env.js";
 import postgres from "postgres";
 
-console.log("connecting to db with", env.DATABASE_URL);
 export const connection = postgres(env.DATABASE_URL);
 
 export const db = drizzle(connection, { schema });
